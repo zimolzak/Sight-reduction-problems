5e742cd9a38b107ffed986fa8f6912d96891c38b
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-12-28 15:06:58 -0500

    Function for the calculation of Z and Hc.

--- sight.py
+++ sight.py
@@ -3,32 +3,25 @@
 import ephem
 from math import sin, cos, asin, acos
 
+datelist = ['2016/01/12 18:00:00', '2016/01/12 19:00:00',
+            '2016/01/12 20:00:00', '2016/01/12 21:00:00']
+
+ghalist = []
+
 jackson = ephem.Observer()
 jackson.lat = '42.2458'
 jackson.lon = '-84.4014'
 #jackson.elevation = 303.9 # meters = 997 feet.
 jackson.elevation = 0 # fixme later
 
-jackson.date = '2016/01/12 18:00:00' # 13:00 eastern
-s = ephem.Sun(jackson)
-print jackson.date, "Alt", s.alt, "Azi", s.az
+for d in datelist:
+    jackson.date = d
+    s = ephem.Sun(jackson)
+    print jackson.date, "Alt", s.alt, "Azi", s.az
 
 ###
 
-jackson.date = '2016/01/12 19:00:00'
-s = ephem.Sun(jackson)
-print jackson.date, "Alt", s.alt, "Azi", s.az
-
-jackson.date = '2016/01/12 20:00:00'
-s = ephem.Sun(jackson)
-print jackson.date, "Alt", s.alt, "Azi", s.az
-
-jackson.date = '2016/01/12 21:00:00'
-s = ephem.Sun(jackson)
-print jackson.date, "Alt", s.alt, "Azi", s.az
-
-###
-
+print
 print "sun radius", s.radius
 
 print
@@ -40,31 +33,25 @@ gha = ephem.degrees('87:56.8')
 dec = ephem.degrees('-21:38.7')
 #print "Dec", dec
 
-## Step 1
-lha = ephem.degrees(gha + jackson.lon)
-#print "long", jackson.lon
-#print "LHA", lha
+def altazimuth(gha, dec, long, lat):
+    ## Step 1
+    lha = ephem.degrees(gha + long)
+    ## Step 2
+    S = sin(dec)
+    C = cos(dec) * cos(lha)
+    Hc = ephem.degrees(asin(S * sin(lat) + C * cos(lat)))
+    ## Step 3
+    X = (S * cos(lat) - C * sin (lat)) / cos(Hc)
+    if X > 1:
+        X = 1
+    elif X < -1:
+        X = -1
+    A = ephem.degrees(acos(X))
+    ## Step 4
+    if lha > ephem.degrees('180'):
+        Z = ephem.degrees(A)
+    else:
+        Z = ephem.degrees(ephem.degrees('360') - A)
+    return dict(Hc=Hc, Z=Z)
 
-## Step 2
-S = sin(dec)
-#print "S", S
-C = cos(dec) * cos(lha)
-#print "C", C
-Hc = ephem.degrees(asin(S * sin(jackson.lat) + C * cos(jackson.lat)))
-print "Hc", Hc
-
-## Step 3
-X = (S * cos(jackson.lat) - C * sin (jackson.lat)) / cos(Hc)
-if X > 1:
-    X = 1
-elif X < -1:
-    X = -1
-A = ephem.degrees(acos(X))
-#print "A", A
-
-## Step 4
-if lha > ephem.degrees('180'):
-    Z = ephem.degrees(A)
-else:
-    Z = ephem.degrees(ephem.degrees('360') - A)
-print "Z", Z
+print altazimuth(gha, dec, jackson.lon, jackson.lat)
