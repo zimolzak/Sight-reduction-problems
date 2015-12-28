7a92cc49c555d4a02a309b441471bfab47fdc4bf
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-12-28 14:54:58 -0500

    Finish Hc and Z from GHA and Dec.

--- sight.py
+++ sight.py
@@ -6,7 +6,8 @@ from math import sin, cos, asin, acos
 jackson = ephem.Observer()
 jackson.lat = '42.2458'
 jackson.lon = '-84.4014'
-jackson.elevation = 997 # 997 feet. Correct??
+#jackson.elevation = 303.9 # meters = 997 feet.
+jackson.elevation = 0 # fixme later
 
 jackson.date = '2016/01/12 18:00:00' # 13:00 eastern
 s = ephem.Sun(jackson)
@@ -35,15 +36,35 @@ jackson.date = '2016/01/12 18:00:00' # 13:00 eastern
 print "At", jackson.date, "...."
 
 gha = ephem.degrees('87:56.8')
-print "GHA", gha
+#print "GHA", gha
 dec = ephem.degrees('-21:38.7')
-print "Dec", dec
+#print "Dec", dec
 
+## Step 1
 lha = ephem.degrees(gha + jackson.lon)
-print "long", jackson.lon
-print "LHA", lha
+#print "long", jackson.lon
+#print "LHA", lha
 
+## Step 2
 S = sin(dec)
-print "S", S
+#print "S", S
 C = cos(dec) * cos(lha)
-print "C", C
+#print "C", C
+Hc = ephem.degrees(asin(S * sin(jackson.lat) + C * cos(jackson.lat)))
+print "Hc", Hc
+
+## Step 3
+X = (S * cos(jackson.lat) - C * sin (jackson.lat)) / cos(Hc)
+if X > 1:
+    X = 1
+elif X < -1:
+    X = -1
+A = ephem.degrees(acos(X))
+#print "A", A
+
+## Step 4
+if lha > ephem.degrees('180'):
+    Z = ephem.degrees(A)
+else:
+    Z = ephem.degrees(ephem.degrees('360') - A)
+print "Z", Z
