d5025f715526f90e7f9edebbc43ecacebca3f033
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-12-28 15:32:45 -0500

    Loop through four hours of almanac data.

--- sight.py
+++ sight.py
@@ -6,32 +6,17 @@ from math import sin, cos, asin, acos
 datelist = ['2016/01/12 18:00:00', '2016/01/12 19:00:00',
             '2016/01/12 20:00:00', '2016/01/12 21:00:00']
 
-ghalist = []
+ghalist = [ephem.degrees('87:56.8'), ephem.degrees('102:56.5'),
+           ephem.degrees('117:56.3'), ephem.degrees('132:56.0')]
+
+declist = [ephem.degrees('-21:38.7'), ephem.degrees('-21:38.3'),
+           ephem.degrees('-21:37.8'), ephem.degrees('-21:38.4')]
 
 jackson = ephem.Observer()
 jackson.lat = '42.2458'
 jackson.lon = '-84.4014'
-#jackson.elevation = 303.9 # meters = 997 feet.
 jackson.elevation = 0 # fixme later
-
-for d in datelist:
-    jackson.date = d
-    s = ephem.Sun(jackson)
-    print jackson.date, "Alt", s.alt, "Azi", s.az
-
-###
-
-print
-print "sun radius", s.radius
-
-print
-jackson.date = '2016/01/12 18:00:00' # 13:00 eastern
-print "At", jackson.date, "...."
-
-gha = ephem.degrees('87:56.8')
-#print "GHA", gha
-dec = ephem.degrees('-21:38.7')
-#print "Dec", dec
+#jackson.elevation = 303.9 # meters = 997 feet.
 
 def altazimuth(gha, dec, long, lat):
     ## Step 1
@@ -54,4 +39,11 @@ def altazimuth(gha, dec, long, lat):
         Z = ephem.degrees(ephem.degrees('360') - A)
     return dict(Hc=Hc, Z=Z)
 
-print altazimuth(gha, dec, jackson.lon, jackson.lat)
+for i in range(len(datelist)):
+    jackson.date = datelist[i]
+    s = ephem.Sun(jackson)
+    print jackson.date, "Alt", s.alt, "Azi", s.az
+    print altazimuth(ghalist[i], declist[i], jackson.lon, jackson.lat)
+
+print
+print "sun radius", s.radius
