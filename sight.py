731b45797262d9c40512246eff0ed42976224627
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-12-29 05:25:23 -0500

    Full example of finding intercept of a real position and an AP.

--- sight.py
+++ sight.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import ephem
-from sr_lib import altazimuth, almanac, ha, ho
+from sr_lib import altazimuth, almanac, ha, ho, intercept
 
 datelist = ['2016/01/12 18:00:00', '2016/01/12 19:00:00',
             '2016/01/12 20:00:00', '2016/01/12 21:00:00']
@@ -16,14 +16,20 @@ ap = jackson.copy()
 ap.lat = '42'
 ap.lon = '-84'
 
+print
+
 for p in [jackson, ap]:
     p.date = datelist[0]
+    print p.date, "UTC"
     s = ephem.Sun(p)
-    print "Hc", s.alt, "Z", s.az
 
-hs_1 = ephem.degrees('26')
-print "hs", hs_1
-ha_1 = ha(hs_1, ephem.degrees('0:1.2'), 'on', 15)
-print "ha", ha_1
-ho_1 = ho(ha_1, ephem.Date(datelist[0]), 'LL')
-print "ho", ho_1
+    hs_1 = ephem.degrees('25:55:01.2')
+    print "hs", hs_1
+    ha_1 = ha(hs_1, ephem.degrees('0:1.2'), 'on', 15)
+    print "ha", ha_1
+    ho_1 = ho(ha_1, ephem.Date(datelist[0]), 'LL')
+    print "ho", ho_1
+    print "calculating at position", p.lat, p.lon
+    print "Hc", s.alt, "/ Z", s.az
+    print "Intercept", intercept(ho_1, s.alt)
+    print
