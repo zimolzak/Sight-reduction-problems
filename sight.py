dc7c674a4b9bc56805868568477a08b31917401c
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-12-29 07:07:27 -0500

    No longer loop over 2 positions. AP only.

--- sight.py
+++ sight.py
@@ -32,30 +32,29 @@ for date_str in datelist:
     jackson.date = date_str
     refsun = ephem.Sun(jackson)
     hs_1 = ho2hs(refsun.alt, ie_ref, arc_ref, eyeht_ref, date_str, limb_ref)
+    print "PROBLEM ----"
     print "hs", hs_1
+    ap.date = date_str
+    print ap.date, "UTC"
+    print
     
     ## Do sight reduction
+    print "SOLUTION ----"
     ha_1 = ha(hs_1, ie_ref, arc_ref, eyeht_ref)
     ho_1 = ho(ha_1, ephem.Date(date_str), limb_ref)
     print "ha", ha_1
     print "ho", ho_1
+    print "calculating at AP", ap.lat, ap.lon
+    s = ephem.Sun(ap)
+    print "Hc", s.alt, "/ Z", s.az
+    I = intercept(ho_1, s.alt)
+    print "Intercept", I
+    if I[1][0] == 'A':
+        theta = s.az - pi
+    elif I[1][0] == 'T':
+        theta = s.az
+    x = destination(ap.lat, ap.lon, theta, I[0])
+    dir1 = ephem.degrees(s.az - pi/2)
+    dir2 = ephem.degrees(s.az + pi/2)
+    print "LOP thru", x.lat, x.lon, "in the", dir1.norm, dir2.norm, "direction"
     print
-    
-    for p in [jackson, ap]:
-        p.date = date_str
-        print p.date, "UTC"
-        s = ephem.Sun(p)
-    
-        print "calculating at position", p.lat, p.lon
-        print "Hc", s.alt, "/ Z", s.az
-        I = intercept(ho_1, s.alt)
-        print "Intercept", I
-        if I[1][0] == 'A':
-            theta = s.az - pi
-        elif I[1][0] == 'T':
-            theta = s.az
-        x = destination(p.lat, p.lon, theta, I[0])
-        dir1 = ephem.degrees(s.az - pi/2)
-        dir2 = ephem.degrees(s.az + pi/2)
-        print "LOP thru", x.lat, x.lon, "in the", dir1.norm, dir2.norm, "direction"
-        print
