cc29262d15793d1949367f6546499e099b0b92ae
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-12-29 07:00:22 -0500

    Big loop over dates. Back-calc seems accurate.

--- sight.py
+++ sight.py
@@ -26,34 +26,36 @@ limb_ref = random.choice(['LL', 'UL'])
 print "IE", ie_ref, arc_ref, "the arc. Eye", eyeht_ref, "meters. Sun", limb_ref
 print
 
-## Set up date- and secret-location-specific back-calculations
-jackson.date = datelist[0]
-refsun = ephem.Sun(jackson)
-hs_1 = ho2hs(refsun.alt, ie_ref, arc_ref, eyeht_ref, datelist[0], limb_ref)
-print "hs", hs_1
+for date_str in datelist:
 
-## Do sight reduction
-ha_1 = ha(hs_1, ie_ref, arc_ref, eyeht_ref)
-ho_1 = ho(ha_1, ephem.Date(datelist[0]), limb_ref)
-print "ha", ha_1
-print "ho", ho_1
-print
-
-for p in [jackson, ap]:
-    p.date = datelist[0]
-    print p.date, "UTC"
-    s = ephem.Sun(p)
-
-    print "calculating at position", p.lat, p.lon
-    print "Hc", s.alt, "/ Z", s.az
-    I = intercept(ho_1, s.alt)
-    print "Intercept", I
-    if I[1][0] == 'A':
-        theta = s.az - pi
-    elif I[1][0] == 'T':
-        theta = s.az
-    x = destination(p.lat, p.lon, theta, I[0])
-    dir1 = ephem.degrees(s.az - pi/2)
-    dir2 = ephem.degrees(s.az + pi/2)
-    print "LOP thru", x.lat, x.lon, "in the", dir1.norm, dir2.norm, "direction"
+    ## Set up date- and secret-location-specific back-calculations
+    jackson.date = date_str
+    refsun = ephem.Sun(jackson)
+    hs_1 = ho2hs(refsun.alt, ie_ref, arc_ref, eyeht_ref, date_str, limb_ref)
+    print "hs", hs_1
+    
+    ## Do sight reduction
+    ha_1 = ha(hs_1, ie_ref, arc_ref, eyeht_ref)
+    ho_1 = ho(ha_1, ephem.Date(date_str), limb_ref)
+    print "ha", ha_1
+    print "ho", ho_1
     print
+    
+    for p in [jackson, ap]:
+        p.date = date_str
+        print p.date, "UTC"
+        s = ephem.Sun(p)
+    
+        print "calculating at position", p.lat, p.lon
+        print "Hc", s.alt, "/ Z", s.az
+        I = intercept(ho_1, s.alt)
+        print "Intercept", I
+        if I[1][0] == 'A':
+            theta = s.az - pi
+        elif I[1][0] == 'T':
+            theta = s.az
+        x = destination(p.lat, p.lon, theta, I[0])
+        dir1 = ephem.degrees(s.az - pi/2)
+        dir2 = ephem.degrees(s.az + pi/2)
+        print "LOP thru", x.lat, x.lon, "in the", dir1.norm, dir2.norm, "direction"
+        print
